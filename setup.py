99a5b305a121b0273598f2f0eb38afddb4088b53
Matthias Blum <mat.blum@gmail.com>
2018-09-19 12:49:34 +0100

    PEP8

--- setup.py
+++ setup.py
@@ -8,7 +8,10 @@ from prontodb import __version__
 
 
 def get_requirements():
-    filepath = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+    filepath = os.path.join(
+        os.path.dirname(__file__),
+        "requirements.txt"
+    )
 
     with open(filepath) as fh:
         requirements = fh.read().splitlines()
@@ -18,7 +21,8 @@ def get_requirements():
 
 setup(
     name='prontodb',
-    description='Refresh Pronto with the latest data from InterPro, GOA, and UniProt',
+    description="Refresh Pronto with the latest data "
+                "from InterPro, GOA, and UniProt",
     version=__version__,
     packages=find_packages(),
     zip_safe=False,
