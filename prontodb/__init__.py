21f5898838611f9c5bb7a6ed3b424c491e7f25f0
Matthias Blum <matthiasblum@users.noreply.github.com>
2018-10-07 17:12:54 +0100

    Add a -p, --thread argument

--- prontodb/__init__.py
+++ prontodb/__init__.py
@@ -36,6 +36,8 @@ def cli():
     parser.add_argument("-t", "--tmpdir", default=gettempdir(),
                         help="temporary directory "
                              "(default: {})".format(gettempdir()))
+    parser.add_argument("-p", "--threads", type=int, default=2,
+                        help="match worker threads (default: 2)")
     parser.add_argument("-o", "--output", default=default_report,
                         help="output SwissProt report for curators "
                              "(default: {})".format(default_report))
@@ -123,6 +125,7 @@ def cli():
             "func": interpro.load_matches,
             "args": (dsn, schema),
             "kwargs": dict(
+                consumers=args.threads,
                 max_gap=max_gap,
                 tmpdir=args.tmpdir
             )
