da295f4dc726d6e6deb5426ff41bd7a6f4c48a80
Matthias Blum <mat.blum@gmail.com>
2018-11-25 23:37:38 +0000

    "mode" argument is obsolete

--- prontload/uniprot.py
+++ prontload/uniprot.py
@@ -102,7 +102,7 @@ def load_comments(dsn, schema, chunk_size=100000):
 
     query = """
         INSERT /*+APPEND*/ INTO {}.COMMENT_VALUE (TOPIC_ID, COMMENT_ID, TEXT)
-        VALUES (:1, :2, :3)    
+        VALUES (:1, :2, :3)
     """.format(schema)
     for i in range(0, len(comments), chunk_size):
         con.executemany(query, comments[i:i+chunk_size])
@@ -110,7 +110,7 @@ def load_comments(dsn, schema, chunk_size=100000):
 
     query = """
         INSERT /*+APPEND*/ INTO {}.PROTEIN_COMMENT (PROTEIN_AC, TOPIC_ID, COMMENT_ID)
-        VALUES (:1, :2, :3)    
+        VALUES (:1, :2, :3)
     """.format(schema)
     for i in range(0, len(protein2comment), chunk_size):
         con.executemany(query, protein2comment[i:i+chunk_size])
@@ -133,7 +133,7 @@ def load_descriptions(dsn, schema, tmpdir=None, chunk_size=100000):
         (
             DESC_ID NUMBER(10) NOT NULL,
             TEXT VARCHAR2(4000) NOT NULL
-        ) NOLOGGING        
+        ) NOLOGGING
         """.format(schema)
     )
 
@@ -143,7 +143,7 @@ def load_descriptions(dsn, schema, tmpdir=None, chunk_size=100000):
         (
             PROTEIN_AC VARCHAR2(15) NOT NULL,
             DESC_ID NUMBER(10) NOT NULL
-        ) NOLOGGING        
+        ) NOLOGGING
         """.format(schema)
     )
 
@@ -162,9 +162,9 @@ def load_descriptions(dsn, schema, tmpdir=None, chunk_size=100000):
                 AND E.FIRST_PUBLIC IS NOT NULL
         )
         WHERE R = 1
-        ORDER BY DESCR    
+        ORDER BY DESCR
     """
-    with io.Store(dir=tmpdir, mode="wb") as store:
+    with io.Store(dir=tmpdir) as store:
         descriptions = []
         proteins = set()
         _text = None
@@ -209,7 +209,7 @@ def load_descriptions(dsn, schema, tmpdir=None, chunk_size=100000):
             con.executemany(
                 """
                 INSERT /*+ APPEND */ INTO {}.DESC_VALUE (DESC_ID, TEXT)
-                VALUES (:1, :2)            
+                VALUES (:1, :2)
                 """.format(schema),
                 cv_table
             )
@@ -222,7 +222,7 @@ def load_descriptions(dsn, schema, tmpdir=None, chunk_size=100000):
                     INSERT /*+ APPEND */ INTO {}.PROTEIN_DESC (
                         PROTEIN_AC, DESC_ID
                     )
-                    VALUES (:1, :2)                
+                    VALUES (:1, :2)
                     """.format(schema),
                     rel_table[i:i+chunk_size]
                 )
@@ -256,7 +256,7 @@ def load_enzymes(dsn, schema):
         (
             PROTEIN_AC VARCHAR2(15) NOT NULL,
             ECNO VARCHAR2(15) NOT NULL
-        ) NOLOGGING        
+        ) NOLOGGING
         """.format(schema)
     )
 
@@ -285,7 +285,7 @@ def load_enzymes(dsn, schema):
 
     con.execute(
         """
-        CREATE INDEX I_ENZYME$EC 
+        CREATE INDEX I_ENZYME$EC
         ON {}.ENZYME (ECNO) NOLOGGING
         """.format(schema)
     )
